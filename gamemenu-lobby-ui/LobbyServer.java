7085dc72757ef5618fd9534f37a3f70fa1d2c63b
Bexanderthebex <melbex11@gmail.com>
2017-12-02 21:47:30 +0800

    game start UI without server connection

--- gamemenu-lobby-ui/LobbyServer.java
+++ gamemenu-lobby-ui/LobbyServer.java
@@ -5,35 +5,60 @@ import java.awt.event.*;
 public class LobbyServer extends JPanel implements ActionListener {
   /* Declarations */
   private static JButton startGame;
+  private static JButton connect;
+  private static JLabel ipField;
+  private static JLabel portField;
   public static JRadioButton ninjaSelector;
   public static JRadioButton samuraiSelector;
   private static JComboBox<String> lives;
   private static JComboBox<String> wins;
-  private static JTextArea chatArea;
   private static JPanel classSelectorContainer;
   private static JPanel gameSettings;
+  private static JPanel chatContainer;
+  private static JTextField ip;
+  private static JTextField port;
+  private static Connections connections;
+
+  //insert instance of chat with ui chat here
 
   /* final values */
 
   public static void main(String[] args) {
     //call the constructor to initialize class
-    new LobbyServer().getPanel();
   }
 
-  public LobbyServer() {
+  // public LobbyServer(int string) {
+  public LobbyServer(String mode){
+    //initializations
     super.setLayout(null);
+    classSelectorContainer = new JPanel();
+    gameSettings = new JPanel();
+    chatContainer = new JPanel();
+    startGame = new JButton("Start Game!");
+    connect = new JButton("Connect");
+    if(mode == "Create a Lobby"){
+      ip = new JTextField("127.0.0.1");
+    } else {
+      ip = new JTextField();
+    }
+    port = new JTextField();
+    ipField = new JLabel("Enter IP");
+    portField = new JLabel("Enter Port");
+    connections = new Connections();
 
-    //instantiate components
+    /* instantiate components */
+
+    //class selector
     ninjaSelector = new JRadioButton("Ninja");
     samuraiSelector = new JRadioButton("Samurai");
-    classSelectorContainer = new JPanel();
     classSelectorContainer.setBorder(BorderFactory
       .createTitledBorder(BorderFactory.createEtchedBorder(), "Select Class"));
+    //game settings
     lives = new JComboBox<String> (new String[] { "3", "4", "5" });
     wins = new JComboBox<String>(new String[] {"3", "4", "5"});
-    gameSettings = new JPanel();
     gameSettings.setBorder(BorderFactory
       .createTitledBorder(BorderFactory.createEtchedBorder(), "Game Settings"));
+
 
     //set component properties 
     ninjaSelector.setActionCommand("Ninja");
@@ -46,12 +71,24 @@ public class LobbyServer extends JPanel implements ActionListener {
     wins.setEditable(true);
     lives.setPreferredSize(new Dimension(100, 20));
     wins.setPreferredSize(new Dimension(100, 20));
+    startGame.setBounds(470, 660, 200, 40);
+    ip.setBounds(40, 100, 150, 30);
+    port.setBounds(220, 100, 150, 30);
+    ipField.setLocation(90, 70);
+    ipField.setSize(60, 30);
+    portField.setLocation(260, 70);
+    portField.setSize(90, 30);
+    connect.setBounds(130, 140, 150, 30);
+    connections.setBounds(40, 200, 350, 350);
 
     //customize JPanels
     classSelectorContainer.setBounds(400, 95, 150, 80);
     classSelectorContainer.setVisible(true);
     gameSettings.setBounds(400, 200, 150, 80);
     gameSettings.setVisible(true);
+    chatContainer.setBackground(Color.BLACK);
+    chatContainer.setBounds(400, 300, 350, 350);
+    chatContainer.setVisible(true);
 
     //place components
     classSelectorContainer.add(ninjaSelector);
@@ -64,14 +101,20 @@ public class LobbyServer extends JPanel implements ActionListener {
     samuraiSelector.addActionListener(this);
     lives.addActionListener(this);
     wins.addActionListener(this);
-    
-    // add to containers
+    startGame.addActionListener(this);
 
-
+    // add to cosntainers
     super.add(classSelectorContainer);
     super.add(gameSettings);
-    // super.add(ninjaSelector);
-    // super.add(samuraiSelector);
+    super.add(chatContainer);
+    super.add(startGame);
+    super.add(ip);
+    super.add(port);
+    super.add(ipField);
+    super.add(portField);
+    super.add(connect);
+    super.add(connections);
+
   }
 
   public JPanel getPanel() {
@@ -86,3 +129,9 @@ public class LobbyServer extends JPanel implements ActionListener {
     super.paintComponent(g);
   }
 }
+
+
+/* 
+ * references 
+ * - java docs
+*/
